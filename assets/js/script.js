35c977b803c9ba1db55e6397f6db4543e38b9367
Jasmina Bihel <jasmina.bihel@gmail.com>
2023-04-03 22:13:04 +0000

    Add event listener to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,49 @@
-function runGame(){
+document.addEventListener("DOMContentLoaded", function () {
+        let buttons = document.getElementsByTagName("button");
+
+        for (let button of buttons) {
+            button.addEventListener("click", function () {
+                if (this.getAttribute("data-type") === "submit") {
+                    alert("You clicked submit");
+                } else {
+                    let gameType = this.getAttribute("data-type")
+                    alert(`You clicked ${gameType}`);
+                }
+            })
+        }
+    }
+
+)
+
+
+function runGame() {
 
 }
-function checkAnswer(){
+
+function checkAnswer() {
 
 }
-function calculateCorrectAnswer(){
+
+function calculateCorrectAnswer() {
 
 }
-function incrementScore(){
+
+function incrementScore() {
 
 }
-function incrementWrongAnswer(){
+
+function incrementWrongAnswer() {
 
 }
-function dispalyAdditionQuestion(){
+
+function dispalyAdditionQuestion() {
 
 }
-function dispalySubtractQuestion(){
-    
+
+function dispalySubtractQuestion() {
+
 }
-function dispalyMultiplyQuestion(){
-    
+
+function dispalyMultiplyQuestion() {
+
 }
