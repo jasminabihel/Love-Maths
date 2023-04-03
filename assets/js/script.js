2a7ae92047f46ae7801eace60ae7c00b593c5b5a
Jasmina Bihel <jasmina.bihel@gmail.com>
2023-04-03 23:32:42 +0000

    Add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     runGame("addition");
     runGame("multiply");
+    runGame("subtract")
 
 });
 
@@ -28,6 +29,8 @@ function runGame(gameType) {
     } else if (gameType === "multiply") {
         dispalyMultiplyQuestion(num1, num2);
 
+    } else if (gameType === "subtract") {
+        dispalySubtractQuestion(num1,num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -62,6 +65,9 @@ function calculateCorrectAnswer() {
 
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
+
+    } else if(operator === "-") {
+        return [operand1 - operand2, "subtract"];
     }
     else {
         alert(`Unimplemented operator ${operator}`);
@@ -90,7 +96,11 @@ function dispalyAdditionQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "+";
 }
 
-function dispalySubtractQuestion() {
+function dispalySubtractQuestion(operand1, operand2) {
+
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
 
 }
 
