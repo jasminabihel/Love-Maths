8eb9d283a43bcbb7a54172070ec155687218de29
Jasmina Bihel <jasmina.bihel@gmail.com>
2023-04-03 22:37:58 +0000

    display addition question to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,32 @@
-document.addEventListener("DOMContentLoaded", function () {
-        let buttons = document.getElementsByTagName("button");
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
 
-        for (let button of buttons) {
-            button.addEventListener("click", function () {
-                if (this.getAttribute("data-type") === "submit") {
-                    alert("You clicked submit");
-                } else {
-                    let gameType = this.getAttribute("data-type")
-                    alert(`You clicked ${gameType}`);
-                }
-            })
-        }
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
+            }
+        });
     }
 
-)
+    runGame("addition");
 
+});
 
-function runGame() {
+function runGame(gameType) {
 
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        dispalyAdditionQuestion(num1,num2);
+    }else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 
 }
 
@@ -39,8 +46,11 @@ function incrementWrongAnswer() {
 
 }
 
-function dispalyAdditionQuestion() {
+function dispalyAdditionQuestion(operand1, operand2) {
 
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function dispalySubtractQuestion() {
